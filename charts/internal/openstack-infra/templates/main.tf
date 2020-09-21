4e18493c4080f1c4ac1a48ae9a0e80e158821b4d
Manuel Ganter <manuel.ganter@hotmail.de>
2020-09-21 16:22:20 +0200

    added quote to subnetpool id

--- charts/internal/openstack-infra/templates/main.tf
+++ charts/internal/openstack-infra/templates/main.tf
@@ -83,7 +83,7 @@ resource "openstack_networking_subnet_v2" "cluster-v6" {
   dns_nameservers = []
 
   {{ if .Values.networks.subnetPoolID }}
-  subnetpool_id = {{ required "subnetPoolID must be nil or valid" .Values.networks.subnetPoolID }}
+  subnetpool_id = {{ required "subnetPoolID must be nil or valid" .Values.networks.subnetPoolID | quote }}
   {{- end}}
 }
 {{- end}}
@@ -109,7 +109,7 @@ resource "openstack_networking_subnet_v2" "services-v6" {
   dns_nameservers = []
 
   {{ if .Values.networks.subnetPoolID }}
-  subnetpool_id = {{ required "subnetPoolID must be nil or valid" .Values.networks.subnetPoolID }}
+  subnetpool_id = {{ required "subnetPoolID must be nil or valid" .Values.networks.subnetPoolID | quote }}
   {{- end}}
 }
 {{- end}}
@@ -128,7 +128,7 @@ resource "openstack_networking_subnet_v2" "pods-v6" {
   dns_nameservers = []
 
   {{ if .Values.networks.subnetPoolID }}
-  subnetpool_id = {{ required "subnetPoolID must be nil or valid" .Values.networks.subnetPoolID }}
+  subnetpool_id = {{ required "subnetPoolID must be nil or valid" .Values.networks.subnetPoolID | quote }}
   {{- end}}
 }
 {{- end}}
